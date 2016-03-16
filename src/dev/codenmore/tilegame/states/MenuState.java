04f556c0ac1cb812ebb11e37d6f01973c1b78052
kammitama5 <krystal.maughan@gmail.com>
2016-03-16 12:07:56 -0700

    continuation; need to troubleshoot backgrounds
    So I it runs, but I need to either fix or insert my backgrounds
    properly: 3/16/16

---
+++ src/dev/codenmore/tilegame/states/MenuState.java
@@ -0,0 +1,25 @@
+package dev.codenmore.tilegame.states;
+
+import java.awt.Graphics;
+
+import dev.codenmore.tilegame.Game;
+
+public class MenuState extends State {
+
+	
+	public MenuState(Game game){
+		super(game);
+	}
+	
+	public void tick() {
+		
+		
+	}
+
+	@Override
+	public void render(Graphics g) {
+		
+		
+	}
+
+}
---
+++ src/dev/codenmore/tilegame/worlds/World.java
@@ -0,0 +1,57 @@
+package dev.codenmore.tilegame.worlds;
+
+import java.awt.Graphics;
+
+import dev.codenmore.tilegame.Game;
+import dev.codenmore.tilegame.tiles.Tile;
+import dev.codenmore.tilegame.utils.Utils;
+
+public class World {
+	
+	private Game game;
+	private int width, height;
+	private int spawnX, spawnY;
+	private int[][] tiles;
+	
+	public World(Game game, String path){
+		this.game = game;
+		loadWorld(path);
+		this.game = game;
+	}
+
+	public void tick(){
+		
+	}
+	
+	public void render(Graphics g){
+		for (int y = 0; y < height; y++){
+			for (int x = 0; x < width; x++){
+				getTile(x, y).render(g, (int)( x * Tile.TILEWIDTH - game.getGameCamera().getxOffset()), (int)(y * Tile.TILEHEIGHT - game.getGameCamera().getyOffset()) );
+			}
+		}
+	}
+	
+	public Tile getTile(int x, int y){
+		Tile t = Tile.tiles[tiles[x][y]];
+		if (t == null)
+			return Tile.dirtTile;
+		return t;
+	}
+	public void loadWorld(String path){
+		String file = Utils.loadFileAsString(path);
+		String[] tokens = file.split("\\s+");
+		width = Utils.parseInt(tokens[0]);
+		width = Utils.parseInt(tokens[1]);
+		spawnX = Utils.parseInt(tokens[2]);
+		spawnY = Utils.parseInt(tokens[3]);
+		
+		tiles = new int[width][height];
+		for (int y = 0; y < height; y++){
+			for (int x = 0; x < width; x++){
+				tiles[x][y] = Utils.parseInt(tokens[(x + y * width) + 4]);
+			}
+		}
+		
+	}
+	
+}
